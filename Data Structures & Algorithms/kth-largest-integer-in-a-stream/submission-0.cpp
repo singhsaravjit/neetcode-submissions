aa49b09a70a9d339e409eb0334f7b87008ce1316
Saravjit Singh <60816679+singhsaravjit@users.noreply.github.com>
2026-06-13 19:03:16 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,29 @@
+class KthLargest {
+
+    private:
+
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minheap;
+public:
+    KthLargest(int k, vector<int>& nums) {
+
+        this->k=k;
+        for(int num : nums){
+            minheap.push(num);
+            if(minheap.size()>k){
+                minheap.pop();
+            }
+        }
+        
+    }
+    
+    int add(int val) {
+        minheap.push(val);
+
+        if(minheap.size()>k){
+            minheap.pop();
+        }
+        return minheap.top();
+        
+    }
+};
