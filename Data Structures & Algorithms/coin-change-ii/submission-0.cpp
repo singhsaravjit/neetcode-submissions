c028ef2d157c69b20974f68a1a982dd1f335f95c
Saravjit Singh <60816679+singhsaravjit@users.noreply.github.com>
2026-07-28 16:28:20 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+       vector<uint> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int i = coins.size() - 1; i >= 0; i--) {
+            for (int a = 1; a <= amount; a++) {
+                dp[a] = dp[a] + (coins[i] <= a ? dp[a - coins[i]] : 0);
+            }
+        }
+        return dp[amount]; 
+    }
+};
